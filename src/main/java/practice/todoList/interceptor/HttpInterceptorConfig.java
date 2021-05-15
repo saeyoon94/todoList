6b25a2ba1e5b350c3979baa8e2692d48ca698956
saeyoon94 <37877831+saeyoon94@users.noreply.github.com>
2021-05-16 00:34:21 +0900

    Create HttpInterceptorConfig.java

---
+++ src/main/java/practice/todoList/interceptor/HttpInterceptorConfig.java
@@ -0,0 +1,20 @@
+package practice.todoList.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class HttpInterceptorConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(sessionInterceptor)
+                .addPathPatterns("/api/**");
+    }
+}
